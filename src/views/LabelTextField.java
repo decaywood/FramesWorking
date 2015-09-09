d0938aeda56f92f0ccd261618119cbcc03602532
decaywood <decaywood@qq.com>
2015-09-09 20:36:54 +0800

    增加了新增航迹上部分界面以及下部分界面

---
+++ src/views/LabelTextField.java
@@ -0,0 +1,22 @@
+package views;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author: decaywood
+ * @date: 2015/9/9 16:53
+ */
+public class LabelTextField extends JPanel {
+
+    public LabelTextField(String lbName, int len) {
+        setPreferredSize(new Dimension(len, 50));
+        setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        JLabel label = new JLabel(lbName);
+        JTextField textField = new JTextField();
+        label.setPreferredSize(new Dimension(len, 25));
+        textField.setPreferredSize(new Dimension(len, 25));
+        add(label);
+        add(textField);
+    }
+}
