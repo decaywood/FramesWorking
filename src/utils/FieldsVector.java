ca968c1092960ea04cbff5db3f9703c9567f8f89
decaywood <decaywood@qq.com>
2015-09-20 15:11:13 +0800

    修改FieldsVector泛型

--- src/utils/FieldsVector.java
+++ src/utils/FieldsVector.java
@@ -13,11 +13,12 @@ public class FieldsVector<T> extends Vector<T> {
     public Map<String, String> fieldsMap;
 
 
-
-
     public FieldsVector() {
         this.fieldsMap = new HashMap<>();
     }
 
+    public void put(String fieldName, String fieldVal) {
+        fieldsMap.put(fieldName, fieldVal);
+    }
 
 }
